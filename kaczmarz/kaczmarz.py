c8ab8b923b8ca72ab03e3df197d0360460ec43b1
lightningleaf <lightningleaf0@gmail.com>
2016-07-06 16:56:01 -0700

    Remove unnecessary imports

--- kaczmarz/kaczmarz.py
+++ kaczmarz/kaczmarz.py
@@ -1,20 +1,6 @@
-from recordclass import recordclass
-import numpy as np
-
 def on(sys, itrs):
 	for i in itrs:
 		row = sys.A[i, :]
 		rej = (row @ sys.x - sys.b[i]) / (row @ row) * row
 		sys.x -= rej
 	return np.copy(x)
-
-
-A = np.array([[1, 0], [-1, 1]])
-x = np.zeros(2) 
-b = np.array([2, 2])
-
-linsys = recordclass('linsys', 'A x b')
-
-sys = linsys(A, x, b)
-kaczmarz.kaczmarz(sys, [0])
-print(x)
