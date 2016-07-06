058d65d9315f623803efd14d8818666b537f0271
lightningleaf <lightningleaf0@gmail.com>
2016-07-05 19:31:42 -0700

    setup.py template

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='1',
+      description='',
+      url='https://github.com/lightningleaf/kaczmarz',
+      author='lightningleaf',
+      author_email='lightningleaf0@gmail.com',
+      #license='',
+      packages=['kaczmarz'],
+      zip_safe=False)
