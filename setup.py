883b7c4bd713ae371875f0d9da080016c5867bd3
lightningleaf <lightningleaf0@gmail.com>
2016-07-05 20:19:22 -0700

    Pypi packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from setuptools import setup
 
-setup(name='funniest',
-      version='1',
-      description='',
-      url='https://github.com/lightningleaf/kaczmarz',
-      author='lightningleaf',
-      author_email='lightningleaf0@gmail.com',
-      #license='',
-      packages=['kaczmarz'],
-      zip_safe=False)
+setup(name='kaczmarz',
+	version='1',
+	description='',
+	url='https://github.com/lightningleaf/kaczmarz',
+	author='lightningleaf',
+	author_email='lightningleaf0@gmail.com',
+	license='N/A',
+	packages=['kaczmarz'],
+	zip_safe=False,
+	classifiers=[
+				# How mature is this project? Common values are
+				#   3 - Alpha
+				#   4 - Beta
+				#   5 - Production/Stable
+				'Development Status :: 3 - Alpha',
+
+				# Indicate who your project is intended for
+				'Intended Audience :: Developers',
+				'Topic :: Software Development :: Build Tools',
+
+				# Pick your license as you wish (should match "license" above)
+				'License :: OSI Approved :: MIT License',
+
+				# Specify the Python versions you support here. In particular, ensure
+				# that you indicate whether you support Python 2, Python 3 or both.
+				'Programming Language :: Python :: 3.5',
+				]
+				)
