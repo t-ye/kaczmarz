58120c9f821681694c8bc604649be48854b04189
lightningleaf <lightningleaf0@gmail.com>
2016-07-06 18:28:16 -0700

    Add optimization to on(), implement itrs()

--- kaczmarz/kaczmarz.py
+++ kaczmarz/kaczmarz.py
@@ -1,9 +1,18 @@
 import numpy as np
 from recordclass import recordclass
 
+
 def on(sys, itrs):
+	A, x, b = sys
+	for i in itrs:
+		row = A[i, :]
+		rej = (row @ x - b[i]) / (row @ row) * row
+		x -= rej
+	return np.copy(x)
+
+def itrs(sys, itrs):
 	for i in itrs:
 		row = sys.A[i, :]
 		rej = (row @ sys.x - sys.b[i]) / (row @ row) * row
 		sys.x -= rej
-	return np.copy(sys.x)
+		yield np.copy(sys.x)
