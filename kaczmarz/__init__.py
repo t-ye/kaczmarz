c35d18220ed266798555464e5f3c9cc96d436dd0
lightningleaf <lightningleaf0@gmail.com>
2016-07-11 16:12:55 -0700

    Move main functions from kaczmarz.py to here for simpler imports

---
+++ kaczmarz/__init__.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def on(sys, idxs):
+	A,x,b = sys
+	for i in idxs:
+		row = A[i,:]
+		x -= (row @ x - b[i]) / (row @ row) * row
+
+def gen(sys, idxs):
+	A,x,b = sys
+	for i in idxs:
+		row = A[i,:]
+		x -= (row @ x - b[i]) / (row @ row) * row
+		yield np.copy(x)
