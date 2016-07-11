2388a4059b05ca0e1990e34c0c7f3fa7dd7c1fd6
lightningleaf <lightningleaf0@gmail.com>
2016-07-11 16:55:21 -0700

    Create generators for row indicies of a matrix for input into Kaczmarz algorithm

---
+++ kaczmarz/rowitr.py
@@ -0,0 +1,15 @@
+import random
+
+def itr(A):
+	rows = len(A)
+	return (i % rows for i in it.count())
+
+def rndu(A):
+	rows = len(A)
+	i = random.randrange(rows)
+	last = None
+	while 1:
+		yield i
+		last,i = i,random.randrange(rows - 1)
+		if i >= last:
+			i += 1
