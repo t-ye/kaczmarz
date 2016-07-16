e97533e367e6571f39d40cac971953ca2fb8211f
lightningleaf <lightningleaf0@gmail.com>
2016-07-16 15:29:03 -0700

    Add weighted row iteration

--- kaczmarz/rowitr.py
+++ kaczmarz/rowitr.py
@@ -1,5 +1,7 @@
-import random
 import itertools as it
+
+from kaczmarz import util
+from random import randrange
 
 def itr(A):
 	rows = len(A)
@@ -7,10 +9,20 @@ def itr(A):
 
 def rndu(A):
 	rows = len(A)
-	i = random.randrange(rows)
-	last = None
+	last,i = None,randrange(rows)
 	while 1:
 		yield i
-		last,i = i,random.randrange(rows - 1)
+		last,i = i,randrange(rows - 1)
 		if i >= last:
 			i += 1
+
+def rndw(A):
+	row_norms = np.empty(len(A))
+	for i in range(len(A)):
+		row_norms[i] = sum(n for n in A[i,:])
+	dist = row_norms / sum(row_norms)
+	return util.rnd(dist)
+
+A = (np.arange(9) + 1).reshape(3, 3)
+print(A)
+print(rndw(A))
