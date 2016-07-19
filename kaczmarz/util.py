a87d6b40de5bc3179f72fd96a5f2ca98872f320d
lightningleaf <lightningleaf0@gmail.com>
2016-07-18 19:30:56 -0700

    Create cumdist_bisect_skip ; Fix cumdist_bisect_skip

--- kaczmarz/util.py
+++ kaczmarz/util.py
@@ -8,52 +8,32 @@
 from bisect import bisect
 from numpy import cumsum
 
+from random import randrange
 from random import uniform
 
-def bisect_nd(dist, p, last, div):
-	# pr - sublist of cumulative probability p tha tneeds to be (r)educed to avoid dl
+def cumdist_bisect_skip(p, skipi):
+	# pr - sublist of cumulative probability p that needs to be (r)educed to avoid dl
 	# dl - probability at the (l)ast index
 	# pl - cumulative probability at the (l)ast index
-	if last == None:
-	pr = p[last+1:]
-	dl = dist[last]
+	pr = p[skipi+1:]
+
+	# Stores the probability density contributed at skipi. 
+	pl = p[skipi]
+	# Calculates the probability density contributed at skipi.
+	dl = pl - (skipi > 0) * p[skipi-1] # don't subtract if skipi == 0
+	
+	# Effectively removes the probability density contributed at skipi.
+	p[skipi] = p[skipi-1] if skipi > 0 else p[skipi+1]
 	pr -= dl
+	# Performs the bisection.
+	i = bisect(p, uniform(0, p[-1])) # div * (p[-1] - dl) / p[-1]
+	if i == skipi:
+		# Takes into account that, if i == skipi, i actually represents an adjacent cumulative probability.
+		i += -1 if skipi > 0 else 1
 
-	pl = p[last]
-	p[last] = p[last-1]
-
-	i = bisect(p, div - div * dl) # div * (p[-1] - dl)
-	if i == last:
-		if i == 0:
-			i == len(p) 
-		i -= 1 # i = (i - 1) % len(p)
-
-	p[last] = pl
+	# Restores the probability density contributed at skipi.
+	p[skipi] = pl
 	pr += dl
+
 	return i
 
-def prob_dist_gen(dist):
-	p = cumsum(dist)
-	while 1:
-		yield i
-		i = bisect_nd(dist, p, i, uniform(0, p[-1]))
-
-# def test():
-# 	dist = [.1, .2, .4, .3]
-# 	p = cumsum(dist)
-# 	# P = np.copy(p)
-# 	last = 1
-# 	gen = (bisect_nd(dist, p, last, rand()) for _ in range(1000))
-# 	for _ in gen:
-# 		pass
-
-# # test()
-# # print('\n(', round(time() - t, 5), ' s)', sep='')
-
-# setup += '''
-# from __main__ import test
-# 	'''
-# print(min(timeit.Timer('test()', setup=setup).repeat(10,5)))
-
-
-# # print('\n(', round(time() - t, 5), ' s)', sep='')
