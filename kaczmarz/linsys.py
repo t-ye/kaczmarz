6574415bfaaf4108936e2e8573b826a689cc7522
lightningleaf <lightningleaf0@gmail.com>
2016-07-22 00:59:28 -0700

    Add linear system factory

---
+++ kaczmarz/linsys.py
@@ -0,0 +1,9 @@
+from recordclass import recordclass
+
+from numpy.random import randn
+linsys = recordclass('linsys', 'A x b')
+
+def normal(rows, cols):
+	sys = linsys(randn(rows, cols), randn(cols, 1), None)
+	sys.b = sys.A @ sys.x
+	return sys	
