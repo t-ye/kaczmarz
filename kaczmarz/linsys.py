a9e26c9c9e6119490352b7ae291ba45c81b92fdf
lightningleaf <lightningleaf0@gmail.com>
2016-07-22 19:52:16 -0700

    Switch to generation of true vectors (vs. nx1 matrices)

--- kaczmarz/linsys.py
+++ kaczmarz/linsys.py
@@ -5,6 +5,6 @@ from numpy.random import randn
 linsys = recordclass('linsys', 'A x b')
 
 def normal(rows, cols):
-	sys = linsys(randn(rows, cols), randn(cols, 1), None)
+	sys = linsys(randn(rows, cols), randn(cols), None)
 	sys.b = sys.A @ sys.x
 	return sys
