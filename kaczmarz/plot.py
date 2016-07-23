cd83ab316e16e8025664b29a7b288e0548fb19df
lightningleaf <lightningleaf0@gmail.com>
2016-07-23 10:41:37 -0700

    Optimize with vectorized relative norm calculation (in util.py) ; show plot before end of build ; allow multiple plots

--- kaczmarz/plot.py
+++ kaczmarz/plot.py
@@ -9,7 +9,9 @@ from . import util
 from matplotlib import pyplot as plt
 
 def itrplot():
+	plt.figure()
 	sys = linsys.normal(1000, 10)
 	X,sys.x = sys.x,np.zeros(10)
 	kgen = gen(sys, [1,2,3])
-	plt.plot([1] + [util.relnorm(X, x) for x in kgen])
+	plt.plot(util.relnorm(X, list(kgen)))
+	plt.show(block=False)
