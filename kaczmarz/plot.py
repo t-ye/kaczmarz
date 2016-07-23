274ac263f3f4466c8afa401ac1f4ba572fe591ee
lightningleaf <lightningleaf0@gmail.com>
2016-07-23 11:03:34 -0700

    Fix minor formatting issues

--- kaczmarz/plot.py
+++ kaczmarz/plot.py
@@ -5,13 +5,23 @@ import numpy as np
 
 from . import gen
 from . import linsys
+from . import rowitr
 from . import util
+
 from matplotlib import pyplot as plt
 
 def itrplot():
 	plt.figure()
 	sys = linsys.normal(1000, 10)
 	X,sys.x = sys.x,np.zeros(10)
-	kgen = gen(sys, [1,2,3])
+	kgen = gen(sys, range(3))
+	plt.plot(util.relnorm(X, list(kgen)))
+	plt.show(block=False)
+
+def rndplot():
+	plt.figure()
+	sys = linsys.normal(1000, 10)
+	X,sys.x = sys.x,np.zeros(10)
+	kgen = gen(sys, it.islice(rowitr.rnd(sys.A), 3))
 	plt.plot(util.relnorm(X, list(kgen)))
 	plt.show(block=False)
