d0cdf0f106f29b5aae63a8131f23679f5a4c9fce
lightningleaf <lightningleaf0@gmail.com>
2016-07-22 19:52:49 -0700

    Replace dummy itrplot() with basic functionality

--- kaczmarz/plot.py
+++ kaczmarz/plot.py
@@ -1,8 +1,12 @@
-from matplotlib import pyplot as plt
+import numpy 
+import numpy as np
 
+from . import gen
 from . import linsys
+from matplotlib import pyplot as plt
 
 def itrplot():
 	sys = linsys.normal(1000, 10)
-	plt.plot([1,2,3])
-	plt.show()
+	X,sys.x = sys.x,np.zeros(10)
+	kgen = gen(sys, [1,2,3])
+	plt.plot([np.linalg.norm(kgen.__next__() - X) for _ in range(3)])
