6968bd61f8a0ef7fdfb397cee0940fb4ded9da83
lightningleaf <lightningleaf0@gmail.com>
2016-07-22 20:16:54 -0700

    Clean plot generation

--- kaczmarz/plot.py
+++ kaczmarz/plot.py
@@ -1,12 +1,15 @@
+import itertools as it
+
 import numpy 
 import numpy as np
 
 from . import gen
 from . import linsys
+from . import util
 from matplotlib import pyplot as plt
 
 def itrplot():
 	sys = linsys.normal(1000, 10)
 	X,sys.x = sys.x,np.zeros(10)
 	kgen = gen(sys, [1,2,3])
-	plt.plot([np.linalg.norm(kgen.__next__() - X) for _ in range(3)])
+	plt.plot([1] + [util.relnorm(X, x) for x in kgen])
